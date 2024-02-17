a7ae801c22d3ff93a998b1e785657b4e833c5296
02player <you@example.com>
2024-02-17 21:35:44 +0100

    Added support second MedHolo discord Fixed multiple welcome message

--- events/onGuildMemberAdd.js
+++ events/onGuildMemberAdd.js
@@ -1,5 +1,7 @@
-module.exports = (client, welcome_channel, Events) => {
+module.exports = (client, welcome_channel,secondWelcome_channel, Events) => {
+    const {firstGuildId, secondGuildId} = require("../config.json")
     client.on(Events.GuildMemberAdd, async member => {
-        await welcome_channel.send(`Siema ${member.user}! Witamy na serwerze **MedHolo EMS**!\nMamy nadzieję, że zostaniesz z nami na dłużej. 🥼`)
+        if(member.guild.id == firstGuildId) welcome_channel.send(`Siema ${member.user}! Witamy na serwerze **MedHolo EMS**!\nMamy nadzieję, że zostaniesz z nami na dłużej. 🥼`);
+        if(member.guild.id == secondGuildId) secondWelcome_channel.send(`Siema ${member.user}! Witamy na serwerze **MedHolo EMS - 2**!\nMamy nadzieję, że zostaniesz z nami na dłużej. 🥼`);
     });
 }
--- events/onReady.js
+++ events/onReady.js
@@ -1,10 +1,11 @@
 module.exports = (client) => {
-    const {logs_channelId, welcome_channelId} = require("../config.json");
+    const {logs_channelId, welcome_channelId,secondWelcome_channelId} = require("../config.json");
     const findCommand = require("../commands/findCommand.js");
     const onGuildMemberAdd = require("./onGuildMemberAdd.js");
     const {Events, SlashCommandBuilder} = require("discord.js");
     var logs_channel;
     var welcome_channel;
+    var secondWelcome_channel;
     client.on(Events.ClientReady, async () => {
         console.log(`Logged in as ${client.user.tag}`);
         console.log(`Bot działa na serwerach:`)
@@ -14,8 +15,9 @@ module.exports = (client) => {
         const find = new SlashCommandBuilder().setName("find").setDescription("Przeszukuje kanały w poszukiwaniu danej frazy.").addStringOption(option => option.setName("fraza").setDescription("Wyszukiwana fraza.").setRequired(true));
         client.application.commands.create(find);
         welcome_channel = client.channels.cache.get(welcome_channelId)
+        secondWelcome_channel = client.channels.cache.get(secondWelcome_channelId)
         logs_channel = client.channels.cache.get(logs_channelId)
         findCommand(client, logs_channel, Events)
-        onGuildMemberAdd(client, welcome_channel, Events)
+        onGuildMemberAdd(client, welcome_channel,secondWelcome_channel, Events)
     });
 }
